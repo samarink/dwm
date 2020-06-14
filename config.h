50f091bd4ce8c8abb19df1afd6cbd1e7c2f0a82b
Konstantin Samarin <konstsamari@gmail.com>
2020-06-14 16:32:18 +0700

    rice: colors, font

--- config.h
+++ config.h
@@ -12,17 +12,18 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 22;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=11" };
+static const char *fonts[]          = { "firacode:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#2e3440";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#4c566a";
 static const char col_gray3[]       = "#d8dee9";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#5e81ac";
+static const char col_gray5[]       = "#3b4252";
+static const char col_cyan[]        = "#88c0d0";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_gray5, col_cyan  },
 };
 
 /* tagging */
@@ -80,7 +81,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray5, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 /* commands spawned when clicking statusbar, the mouse button pressed is exported as BUTTON */
