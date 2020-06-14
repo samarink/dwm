44e5f6cbe5c3eb3282955e3a7b3790aa73f212df
Konstantin Samarin <konstsamari@gmail.com>
2020-06-15 02:43:01 +0700

    change clipboard manager cmd

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
@@ -13,7 +13,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 22;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "firacode:size=11" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "firacode:size=11";
 static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#4c566a";
 static const char col_gray3[]       = "#d8dee9";
@@ -39,7 +39,7 @@ static const Rule rules[] = {
 	{ "Gimp",          NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox",       NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "kitty",         NULL,     NULL,           0,         0,          1,          -1,        -1 },
-	{ "copyq",         NULL,     NULL,           0,         1,          0,          -1,        -1 },
+	/* { "copyq",         NULL,     NULL,           0,         1,          0,          -1,        -1 }, */
 	{ "Pcmanfm",       NULL,     NULL,           0,         1,          0,          -1,        -1 },
 	{ "Pavucontrol",   NULL,     NULL,           0,         1,          0,          -1,        -1 },
 	{ NULL,            NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
@@ -92,6 +92,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray5, "-sf", col_gray4,
                                                                                 "-nhb", col_gray1, "-nhf", col_green, "-shb", col_gray5, "-shf", col_gray4, NULL };
+static const char *clipmenudcmd[] = { "clipmenu", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray5, "-sf", col_gray4,
+                                                                                "-nhb", col_gray1, "-nhf", col_green, "-shb", col_gray5, "-shf", col_gray4, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 /* commands spawned when clicking statusbar, the mouse button pressed is exported as BUTTON */
@@ -104,16 +106,18 @@ static const char *scratchpadcmd[] = { "kitty", "-T", scratchpadname, "--class",
 #include "shiftview.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	STACKKEYS(MODKEY,                                     focus)
+	STACKKEYS(MODKEY|ShiftMask,                           push)
 	{ MODKEY,                       XK_p,                 spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return,            spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_u,                 togglescratch,  {.v = scratchpadcmd } },
+  { MODKEY,                       XK_y,                 spawn,          {.v = clipmenudcmd } },
   { MODKEY,                       XK_n,                 spawn,          SHCMD("brave") },
   { MODKEY|ShiftMask,             XK_n,                 spawn,          SHCMD("brave --incognito") },
   { MODKEY,                       XK_v,                 spawn,          SHCMD("kitty -e nvim") },
   { MODKEY,                       XK_e,                 spawn,          SHCMD("kitty -e lf") },
   { MODKEY|ShiftMask,             XK_e,                 spawn,          SHCMD("pcmanfm") },
   { MODKEY,                       XK_F1,                spawn,          SHCMD("pavucontrol") },
-  { MODKEY,                       XK_y,                 spawn,          SHCMD("copyq toggle") },
   { 0,                            XK_Print,             spawn,          SHCMD("flameshot gui") },
   { MODKEY|ControlMask,           XK_t,                 spawn,          SHCMD("toggletransperency") },
   { MODKEY,                       XK_F2,                spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5%") },
@@ -122,8 +126,6 @@ static Key keys[] = {
   { MODKEY|ShiftMask,             XK_grave,             spawn,          SHCMD("sudo systemctl suspend") },
   { MODKEY|Mod1Mask,              XK_grave,             spawn,          SHCMD("sudo systemctl hibernate") },
 	{ MODKEY,                       XK_b,                 togglebar,      {0} },
-	STACKKEYS(MODKEY,                                     focus)
-	STACKKEYS(MODKEY|ShiftMask,                           push)
 	{ MODKEY,                       XK_i,                 incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,                 incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,                 setmfact,       {.f = -0.05} },
